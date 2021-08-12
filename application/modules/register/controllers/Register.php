bdc9d29ae83d0636b412b64df6f5fd195346c79c
Rizky Musthofa <rizkymusthofa0509@gmail.com>
2021-08-12 16:21:42 +0700

    Update

---
+++ application/modules/register/controllers/Register.php
@@ -0,0 +1,48 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Register extends CI_Controller {
+ 	function __construct() {
+	    parent::__construct(); 
+
+	    /*Load session*/
+	    // $this->load->library('session');
+	    /*Model*/
+	    // $this->load->model('M_employee');  
+	    // $this->load->helper('dompet_helper');  
+	    // login(); 
+		
+	}
+
+	public function index()
+	{
+		$data['title'] = '';
+		$data['pages'] = '';
+		$this->load->view('main',$data);
+	}
+	
+	 
+
+ 	 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
