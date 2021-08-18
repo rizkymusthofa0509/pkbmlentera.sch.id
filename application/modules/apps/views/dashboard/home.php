43aed543fdfea1844e9d3ba088dc2b142e4a5239
Rizky Musthofa <rizkymusthofa0509@gmail.com>
2021-08-18 14:55:16 +0700

    UP

---
+++ application/modules/apps/views/dashboard/home.php
@@ -0,0 +1,74 @@
+<div class="row">
+    <div class="col-lg-6 col-xl-3">
+        <div class="card">
+            <div class="card-body">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <h6 class="text-uppercase font-size-12 text-muted mb-3">Pendaftaran aktif</h6>
+                        <span class="h3 mb-0"> $85.50 </span>
+                    </div>
+                    <div class="col-auto">
+                        <span class="badge badge-soft-success">+7.5%</span>
+                    </div>
+                </div> <!-- end row -->
+
+                <div id="sparkline1" class="mt-3"></div>
+            </div> <!-- end card-body-->
+        </div> <!-- end card-->
+    </div> <!-- end col-->
+    
+    <div class="col-lg-6 col-xl-3">
+        <div class="card">
+            <div class="card-body">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <h6 class="text-uppercase font-size-12 text-muted mb-3">Total Pendaftar</h6>
+                        <span class="h3 mb-0"> $12,548.25 </span>
+                    </div>
+                    <div class="col-auto">
+                        <span class="badge badge-soft-danger">-24.5%</span>
+                    </div>
+                </div> <!-- end row -->
+
+                <div id="sparkline2" class="mt-3"></div>
+            </div> <!-- end card-body-->
+        </div> <!-- end card-->
+    </div> <!-- end col-->
+
+    <div class="col-lg-6 col-xl-3">
+        <div class="card">
+            <div class="card-body">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <h6 class="text-uppercase font-size-12 text-muted mb-3">Total Account</h6>
+                        <span class="h3 mb-0"> $8,451.28 </span>
+                    </div>
+                    <div class="col-auto">
+                        <span class="badge badge-soft-success">+3.5%</span>
+                    </div>
+                </div> <!-- end row -->
+
+                <div id="sparkline3" class="mt-3"></div>
+            </div> <!-- end card-body-->
+        </div> <!-- end card-->
+    </div> <!-- end col-->
+
+    <div class="col-lg-6 col-xl-3">
+        <div class="card">
+            <div class="card-body">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <h6 class="text-uppercase font-size-12 text-muted mb-3">Total Siswa</h6>
+                        <span class="h3 mb-0"> 1,12,584 </span>
+                    </div>
+                    <div class="col-auto">
+                        <span class="badge badge-soft-success">+53.5%</span>
+                    </div>
+                </div> <!-- end row -->
+
+                <div id="sparkline4" class="mt-3"></div>
+            </div> <!-- end card-body-->
+        </div> <!-- end card-->
+    </div> <!-- end col-->
+</div>
+<!-- end row-->
